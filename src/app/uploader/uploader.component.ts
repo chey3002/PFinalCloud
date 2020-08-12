28f73a8d7a69ddd0c3d28c57ca1969c4913e7743
chey3002 <carlos-avb@hotmail.com>
2020-08-11 23:06:15 -0500

    url button

--- src/app/uploader/uploader.component.ts
+++ src/app/uploader/uploader.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'uploader',
@@ -7,7 +8,9 @@ import { Component } from '@angular/core';
 })
 export class UploaderComponent {
   isHovering: boolean;
-
+  UrlForm = new FormGroup({
+    url: new FormControl(''),
+  });
   files: File[] = [];
 
   toggleHover(event: boolean) {
