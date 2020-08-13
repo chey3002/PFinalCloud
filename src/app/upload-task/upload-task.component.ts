e992729afc49995017b4518ed4ffd503c553deee
David Fernando Valladarez Muñoz <49535179+DavidValladarez95@users.noreply.github.com>
2020-08-12 20:23:02 -0500

    Habilitación de Zona de Caida
    Habilitación de Zona de caida con servicio de Azure

--- src/app/upload-task/upload-task.component.ts
+++ src/app/upload-task/upload-task.component.ts
@@ -6,6 +6,8 @@ import {
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 import { finalize, tap } from 'rxjs/operators';
+import { RestService } from 'rest.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'upload-task',
@@ -20,11 +22,17 @@ export class UploadTaskComponent implements OnInit {
   percentage: Observable<number>;
   snapshot: Observable<any>;
   downloadURL: string;
+  public resultado;
+  public objeto = {};
 
   constructor(
     private storage: AngularFireStorage,
-    private db: AngularFirestore
+    private db: AngularFirestore ,
+    public rest:RestService, 
+    private route: ActivatedRoute, 
+    private router: Router
   ) {}
+ 
 
   ngOnInit() {
     this.startUpload();
@@ -49,12 +57,28 @@ export class UploadTaskComponent implements OnInit {
       finalize(async () => {
         this.downloadURL = await ref.getDownloadURL().toPromise();
 
+        this.objeto = {
+          'URL' : this.downloadURL
+        };
+        
+        this.AnimalSearchFunction();
+
         this.db
           .collection('files')
           .add({ downloadURL: this.downloadURL, path });
       })
     );
   }
+
+  AnimalSearchFunction() {
+    this.rest.AnimalSearch(this.objeto).subscribe((result) => {
+      this.resultado = result
+      console.log(this.resultado)
+    }, (err) => {
+      console.log("ERROR")
+      console.log(err);
+    });
+};
 
   isActive(snapshot) {
     return (
